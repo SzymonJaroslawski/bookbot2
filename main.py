44b843fa4dd562e697e752480a8b84638def8346
kolodziejj <oliwiako94@gmail.com>
2024-12-11 10:09:53 +0100

    add comments

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def main():
 
     print("--- End raport ---")
 
-
+# split text into words and counts them
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-
+# return "num" value of sorting dictionary
 def sort_on(d):
     return d["num"]
 
-
+# convert dictionary into list of it for each letter
 def chars_to_list(disc):
     sorted_list = []
 
@@ -33,7 +33,7 @@ def chars_to_list(disc):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
-
+# count chars in text, convert into small caps and count each chars into dictionary
 def get_num_chars(text):
     chars = {}
     lowered_words = "".join(text).lower()
@@ -46,7 +46,7 @@ def get_num_chars(text):
         
     return chars
 
-
+# open and read file
 def get_book_text(path):
     with open(path) as f:
         return f.read()
